1d735fc52326f1ecc5df9f2a4dead277f2d9e8b2
Nate Ebel <n8ebel@goobar.dev>
2023-05-04 00:20:58 -0600

    demo: Lesson 5

--- compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListScreen.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListScreen.kt
@@ -2,6 +2,7 @@
 
 package dev.goobar.composedemo.versions
 
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,14 +15,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -29,22 +34,33 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import dev.goobar.composedemo.R
 import dev.goobar.composedemo.data.AndroidVersionInfo
-import dev.goobar.composedemo.data.AndroidVersionsRepository
 
 @Composable
-internal fun AndroidVersionsListScreen(onClick: (AndroidVersionInfo) -> Unit) {
+internal fun AndroidVersionsListScreen(viewModel: AndroidVersionsListViewModel = viewModel(), onClick: (AndroidVersionInfo) -> Unit) {
+    val versionsListState by viewModel.state.collectAsState()
+
     Scaffold(
-        topBar = { TopAppBar(title = { Text("Hello Jetpack Compose") }) }
+        topBar = {
+            TopAppBar(
+                title = { Text("Hello Jetpack Compose") },
+                actions = {
+                    IconButton(onClick = viewModel::onSortClicked) {
+                        Icon(painter = painterResource(id = R.drawable.ic_sort), contentDescription = "Sort")
+                    }
+                }
+            )
+        }
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
             LazyColumn(
                 contentPadding = PaddingValues(20.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                items(
-                    items = AndroidVersionsRepository.data,
-                    key = { info -> info.apiVersion }) { info ->
-                    AndroidVersionInfoCard(info, onClick)
+               itemsIndexed(
+                    items = versionsListState.versionsList,
+                    key = { index, viewItem -> "$index ${viewItem.info.apiVersion}" }
+                ) { index, viewItem ->
+                    AndroidVersionInfoCard(viewItem, onClick)
                 }
             }
         }
@@ -53,14 +69,14 @@ internal fun AndroidVersionsListScreen(onClick: (AndroidVersionInfo) -> Unit) {
 
 @Composable
 private fun AndroidVersionInfoCard(
-    info: AndroidVersionInfo,
+    viewItem: AndroidVersionsListViewModel.State.AndroidVersionViewItem,
     onClick: (AndroidVersionInfo) -> Unit
 ) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .defaultMinSize(minHeight = 120.dp)
-            .clickable { onClick(info) }
+            .clickable { onClick(viewItem.info) }
     ) {
         Row(
             modifier = Modifier.padding(20.dp),
@@ -72,11 +88,11 @@ private fun AndroidVersionInfoCard(
                 contentDescription = "Android icon"
             )
             Column(modifier = Modifier.padding(start = 20.dp)) {
-                Text(text = info.publicName, style = MaterialTheme.typography.headlineMedium)
-                Text(text = "${info.codename} - API ${info.apiVersion}")
+                Text(text = viewItem.title, style = MaterialTheme.typography.headlineMedium)
+                Text(text = viewItem.subtitle)
                 Text(
                     modifier = Modifier.padding(top = 4.dp),
-                    text = info.details,
+                    text = viewItem.description,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )

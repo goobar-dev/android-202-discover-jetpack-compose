91a108246af67de37a03077d3de0392022868136
Nate Ebel <n8ebel@goobar.dev>
2023-05-04 04:02:28 -0600

    Revert "Demo code (#3)"
    This reverts commit cf6a9eb0de56cd392e15a5bbd9cf43f9e4167154.

--- compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
@@ -1,15 +1,15 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
-package dev.goobar.composedemo.home
+package dev.goobar.composedemo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import dev.goobar.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +22,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DemoNavigationGraph()
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeDemoTheme {
+        Greeting("Android")
     }
 }
--- compose-demo/app/src/main/java/dev/goobar/composedemo/home/DemoNavigationDestinations.kt
+++
@@ -1,8 +0,0 @@
-package dev.goobar.composedemo.home
-
-import dev.goobar.composedemo.data.AndroidVersionInfo
-
-object DemoNavigationDestinations {
-    object VersionsList : NavigationDestination
-    object VersionDetails : ArgumentDestination<AndroidVersionInfo>
-}

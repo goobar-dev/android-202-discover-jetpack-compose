d0e3f1b27a14cda3f985f36c9d8af6bb86602b44
Nate Ebel <n8ebel@goobar.dev>
2023-05-04 01:36:25 -0600

    demo: Lesson 8 part 1

--- compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListScreen.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListScreen.kt
@@ -78,7 +78,6 @@ private fun AndroidVersionsList(
     ) {
         itemsIndexed(
             items = viewItems,
-            key = { index, viewItem -> "$index ${viewItem.info.apiVersion}" }
         ) { index, viewItem ->
             AndroidVersionInfoCard(viewItem, onClick)
         }

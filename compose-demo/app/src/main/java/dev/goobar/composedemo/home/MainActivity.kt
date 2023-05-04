b3a60453ec37d58e89526171b0b51a29a8ff20e9
Nate Ebel <n8ebel@goobar.dev>
2023-05-03 20:12:50 -0600

    setup: Lesson 4 starter code

---
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/home/MainActivity.kt
@@ -0,0 +1,30 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package dev.goobar.composedemo.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dev.goobar.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainActivityContent()
+                }
+            }
+        }
+    }
+}

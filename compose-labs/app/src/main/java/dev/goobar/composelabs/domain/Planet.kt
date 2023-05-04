c8843a8b78ad925f6e855761ccb245b370daee58
Nate Ebel <n8ebel@goobar.dev>
2023-05-04 03:24:56 -0600

    lab: Lab 2

---
+++ compose-labs/app/src/main/java/dev/goobar/composelabs/domain/Planet.kt
@@ -0,0 +1,16 @@
+package dev.goobar.composelabs.domain
+
+import dev.goobar.composelabs.NavigationArgs
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Planet(
+    val name: String,
+    val rotationPeriod: Int,
+    val orbitalPeriod: Int,
+    val diameter: Int,
+    val climate: String,
+    val gravity: String,
+    val population: String,
+    val url: String
+): NavigationArgs

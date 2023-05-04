cb67e805e125c7b4fd48d0f83d47d04cdac346fe
Nate Ebel <n8ebel@goobar.dev>
2023-05-03 19:27:52 -0600

    demo: Lesson 2

--- compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
@@ -1,16 +1,48 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package dev.goobar.composedemo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.goobar.composedemo.ui.theme.ComposeDemoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Compose")
+                    MainActivityContent()
                 }
             }
         }
@@ -30,17 +62,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MainActivityContent() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Hello Jetpack Compose") })
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            LazyColumn(
+                contentPadding = PaddingValues(20.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(items = AndroidVersionsRepository.data, key = { info -> info.apiVersion }) { info ->
+                    AndroidVersionInfoCard(info) { clickedInfo ->
+                        coroutineScope.launch { snackbarHostState.showSnackbar("Clicked ${clickedInfo.codename}") }
+                    }
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeDemoTheme {
-        Greeting("Kotlin!!")
+private fun AndroidVersionInfoCard(
+    info: AndroidVersionInfo,
+    onClick: (AndroidVersionInfo) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 120.dp)
+            .clickable { onClick(info) }
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = R.drawable.ic_android),
+                contentDescription = "Android icon"
+            )
+            Column(modifier = Modifier.padding(start = 20.dp)) {
+                Text(text = info.publicName, style = MaterialTheme.typography.headlineMedium)
+                Text(text = "${info.codename} - API ${info.apiVersion}")
+                Text(
+                    modifier = Modifier.padding(top = 4.dp),
+                    text = info.details,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
 }

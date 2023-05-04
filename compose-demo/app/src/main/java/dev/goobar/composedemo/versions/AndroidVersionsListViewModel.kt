dbe61f2ee8af5a72aa2d823d78448abcbdb10ddd
Nate Ebel <n8ebel@goobar.dev>
2023-05-04 01:36:25 -0600

    demo: Lesson 8 part 1

--- compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListViewModel.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/versions/AndroidVersionsListViewModel.kt
@@ -1,11 +1,17 @@
 package dev.goobar.composedemo.versions
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.goobar.composedemo.data.AndroidVersionInfo
 import dev.goobar.composedemo.data.AndroidVersionsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.plus
 
 enum class Sort {
     ASCENDING, DESCENDING
@@ -15,15 +21,36 @@ class AndroidVersionsListViewModel : ViewModel() {
 
     private var sort = Sort.ASCENDING
 
-    private val _state = MutableStateFlow(generateState())
-    val state = _state.asStateFlow()
+    private val _state = flow {
+        val items = AndroidVersionsRepository.data
+        val itemsToReturn = mutableListOf<AndroidVersionInfo>()
+
+        items.forEach { info ->
+            delay(4000)
+            itemsToReturn.add(info)
+            emit(
+                State(
+                    itemsToReturn.map { info ->
+                        State.AndroidVersionViewItem(
+                            title = info.publicName,
+                            subtitle = "${info.codename} - API ${info.apiVersion}",
+                            description = info.details,
+                            info = info
+                        )
+                    }
+                )
+            )
+        }
+    }
+    val state = _state.stateIn(viewModelScope + Dispatchers.Main.immediate, SharingStarted.WhileSubscribed(3_000), State(
+        emptyList()))
 
     fun onSortClicked() {
-        sort = when (sort) {
-            Sort.ASCENDING -> Sort.DESCENDING
-            Sort.DESCENDING -> Sort.ASCENDING
-        }
-        _state.update { generateState() }
+//        sort = when (sort) {
+//            Sort.ASCENDING -> Sort.DESCENDING
+//            Sort.DESCENDING -> Sort.ASCENDING
+//        }
+//        _state.update { generateState() }
     }
 
     private fun generateState() = State(

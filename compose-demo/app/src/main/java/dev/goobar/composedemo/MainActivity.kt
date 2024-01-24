7a1b432e76039b7df4af6a0ed2d6f7b28be39a2b
Nate Ebel <n8ebel@goobar.dev>
2024-01-24 13:21:39 -0700

    demo: Lesson 2

--- compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
+++ compose-demo/app/src/main/java/dev/goobar/composedemo/MainActivity.kt
@@ -1,15 +1,41 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package dev.goobar.composedemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import dev.goobar.composedemo.data.AndroidVersionInfo
+import dev.goobar.composedemo.data.AndroidVersionsRepository
 import dev.goobar.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +45,7 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting(
-                        greeting = "Greetings",
-                        name = "Compose"
-                    )
+                    MainActivityContent()
                 }
             }
         }
@@ -30,13 +53,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
-    modifier: Modifier = Modifier,
-    greeting: String = "Hello",
-    name: String = "World"
+private fun MainActivityContent() {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Compose Demo") })
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            val context = LocalContext.current
+            LazyColumn(
+                contentPadding = PaddingValues(20.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(items = AndroidVersionsRepository.data, key = { info -> info.apiVersion }) { info ->
+                    AndroidVersionInfoCard(info) { clickedInfo ->
+                        Toast.makeText(context, "Clicked ${clickedInfo.publicName}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AndroidVersionInfoCard(
+    info: AndroidVersionInfo,
+    onClick: (AndroidVersionInfo) -> Unit
 ) {
-    Text(
-        text = "$greeting $name!",
-        modifier = modifier
-    )
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 120.dp)
+            .clickable { onClick(info) }
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = R.drawable.ic_android),
+                contentDescription = "Android icon"
+            )
+            Column(modifier = Modifier.padding(start = 20.dp)) {
+                Text(text = info.publicName, style = MaterialTheme.typography.headlineMedium)
+                Text(text = "${info.codename} - API ${info.apiVersion}")
+                Text(
+                    modifier = Modifier.padding(top = 4.dp),
+                    text = info.details,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
 }
